f4feeda93f7602b6cb89ef21ae8b12f5e1de1160
Maxr1998 <max.rumpf1998@gmail.com>
2025-02-17 00:30:22 +0100

    TMP

--- lyriks.py
+++ lyriks.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import lyriks
+from lyriks.providers.vibe import Vibe
 
 if __name__ == '__main__':
-    lyriks.cli()
+    #lyriks.cli()
+    Vibe().fetch_album_song_ids(9334427)
+    lyriks.fetch_single_song(Vibe(), 42036917, output_path='test.lrc')
