9bbcbd1bc983203be496e18508f3fe6aa07f0398
Maxr1998 <max.rumpf1998@gmail.com>
2025-12-30 13:58:06 +0100

    Extract Genie API requests to separate file

---
+++ lyriks/providers/api/genie_api.py
@@ -0,0 +1,113 @@
+import json
+from dataclasses import dataclass
+from json.decoder import JSONDecodeError
+from urllib.parse import unquote
+
+import httpx
+
+from lyriks.lyrics import Lyrics
+from lyriks.providers import Song
+
+GENIE_ALBUM_API_URL = 'https://app.genie.co.kr/song/j_AlbumSongList.json?axnm={album_id:d}'
+GENIE_LYRICS_API_URL = 'https://dn.genie.co.kr/app/purchase/get_msl.asp?songid={song_id:d}&callback=GenieCallback'
+GENIE_STREAM_INFO_API_URL = 'https://stm.genie.co.kr/player/j_StmInfo.json?xgnm={song_id:d}'
+
+CURL_USER_AGENT = 'curl/8.7.1'  # for whatever reason, this works, but the python-requests UA doesn't
+
+
+@dataclass
+class GenieSong(Song):
+    pass
+
+
+def get_album_songs(album_id: int) -> list[GenieSong] | None:
+    response = httpx.get(GENIE_ALBUM_API_URL.format(album_id=album_id), headers={'User-Agent': CURL_USER_AGENT})
+    try:
+        response_json = response.json()
+    except JSONDecodeError:
+        return None
+
+    try:
+        songs = list(response_json['DATA1']['DATA'])
+    except KeyError:
+        return None
+
+    # Reject albums with multiple CDs
+    if any(song.get('ALBUM_CD_NO') != '1' for song in songs):
+        return None
+
+    # Extract songs
+    result = []
+
+    for song in songs:
+        song_id = song.get('SONG_ID')
+        track_num = song.get('ALBUM_TRACK_NO')
+        song_name = song.get('SONG_NAME')
+
+        if song_id is None or track_num is None or song_name is None:
+            return None
+
+        try:
+            song_id = int(song_id)
+            track_num = int(track_num)
+        except ValueError:
+            return None
+
+        result.append(GenieSong(id=song_id, album_index=track_num, title=unquote(song_name)))
+
+    result = sorted(result, key=lambda x: x.album_index)
+
+    return result
+
+
+def get_stream_info(song_id: int) -> dict | None:
+    try:
+        return httpx.get(
+            GENIE_STREAM_INFO_API_URL.format(song_id=song_id),
+            headers={'User-Agent': CURL_USER_AGENT},
+        ).json()
+    except JSONDecodeError:
+        return None
+
+
+def get_song_lyrics(song: GenieSong) -> Lyrics | None:
+    # Try to fetch synced lyrics
+    lyrics_response = httpx.get(
+        GENIE_LYRICS_API_URL.format(song_id=song.id),
+        headers={'User-Agent': CURL_USER_AGENT},
+    ).text
+
+    if lyrics_response.startswith('GenieCallback('):
+        # We (probably) got synced lyrics
+        lyrics_response = lyrics_response.removeprefix('GenieCallback(').removesuffix(');')
+        try:
+            raw_lyrics = json.loads(lyrics_response)
+        except JSONDecodeError:
+            return None
+
+        # Convert timestamps and cleanup lines
+        lyrics_dict: dict[int, str] = {int(timestamp): line.strip() for timestamp, line in raw_lyrics.items()}
+
+        return Lyrics.from_dict(song.id, song.title, lyrics_dict)
+    else:
+        # Fall back to static lyrics from stream info
+        stream_info = get_stream_info(song.id)
+        if not stream_info:
+            return None
+
+        try:
+            raw_lyrics = stream_info['DataSet']['DATA'][0]['LYRICS']
+        except KeyError:
+            return None
+
+        # Reject empty lyrics
+        if not raw_lyrics:
+            return None
+
+        lines = unquote(raw_lyrics).split('<br>')
+
+        # Reject instrumental tracks
+        if '이 곡은 연주곡 입니다.' in lines:
+            return None
+
+        return Lyrics(song_id=song.id, song_title=song.title, lines=lines, is_synced=False)
--- lyriks/providers/genie.py
+++ lyriks/providers/genie.py
@@ -1,25 +1,9 @@
-import json
-from dataclasses import dataclass
-from json.decoder import JSONDecodeError
-from urllib.parse import unquote
-
-import httpx
-
 from lyriks.lyrics import Lyrics
 from lyriks.mb_client import Release, Artist
-from lyriks.providers import Provider, Song
-from lyriks.providers.util import pick_release_from_release_group
-
-GENIE_ALBUM_API_URL = 'https://app.genie.co.kr/song/j_AlbumSongList.json?axnm={album_id:d}'
-GENIE_LYRICS_API_URL = 'https://dn.genie.co.kr/app/purchase/get_msl.asp?songid={song_id:d}&callback=GenieCallback'
-GENIE_STREAM_INFO_API_URL = 'https://stm.genie.co.kr/player/j_StmInfo.json?xgnm={song_id:d}'
-
-CURL_USER_AGENT = 'curl/8.7.1'  # for whatever reason, this works, but the python-requests UA doesn't
-
-
-@dataclass
-class GenieSong(Song):
-    pass
+from . import Provider, Song
+from .api import genie_api
+from .api.genie_api import GenieSong
+from .util import pick_release_from_release_group
 
 
 class Genie(Provider):
@@ -42,7 +26,7 @@ class Genie(Provider):
         return self.fetch_provider_song_lyrics(genie_song)
 
     def fetch_song_by_id(self, song_id: int) -> Song | None:
-        stream_info = self.fetch_genie_stream_info(song_id)
+        stream_info = genie_api.get_stream_info(song_id)
         if stream_info is None:
             return None
 
@@ -51,55 +35,14 @@ class Genie(Provider):
         except KeyError:
             return None
 
-        genie_songs = self.fetch_genie_album_songs(album_id)
+        genie_songs = genie_api.get_album_songs(album_id)
         if not genie_songs:
             return None
 
         return next((s for s in genie_songs if s.id == song_id), None)
 
     def fetch_provider_song_lyrics(self, song: GenieSong) -> Lyrics | None:
-        # Fetch stream info with general song info and static lyrics
-        song_id = song.id
-        stream_info = self.fetch_genie_stream_info(song_id)
-        if not stream_info:
-            return None
-
-        # Try to fetch synced lyrics
-        lyrics_response = httpx.get(
-            GENIE_LYRICS_API_URL.format(song_id=song_id),
-            headers={'User-Agent': CURL_USER_AGENT},
-        ).text
-
-        if lyrics_response.startswith('GenieCallback('):
-            # We (probably) got synced lyrics
-            lyrics_response = lyrics_response.removeprefix('GenieCallback(').removesuffix(');')
-            try:
-                raw_lyrics = json.loads(lyrics_response)
-            except JSONDecodeError:
-                return None
-
-            # Convert timestamps and cleanup lines
-            lyrics_dict: dict[int, str] = {int(timestamp): line.strip() for timestamp, line in raw_lyrics.items()}
-
-            return Lyrics.from_dict(song_id, song.title, lyrics_dict)
-        else:
-            # Fall back to static lyrics
-            try:
-                raw_lyrics = stream_info['DataSet']['DATA'][0]['LYRICS']
-            except KeyError:
-                return None
-
-            # Reject empty lyrics
-            if not raw_lyrics:
-                return None
-
-            lines = unquote(raw_lyrics).split('<br>')
-
-            # Reject instrumental tracks
-            if '이 곡은 연주곡 입니다.' in lines:
-                return None
-
-            return Lyrics(song_id=song_id, song_title=song.title, lines=lines, is_synced=False)
+        return genie_api.get_song_lyrics(song)
 
     def has_artist_url(self, artist: Artist) -> bool:
         if artist.has_genie_url:
@@ -128,7 +71,7 @@ class Genie(Provider):
             return None
         matched_release, album_id = result
 
-        genie_songs = self.fetch_genie_album_songs(album_id)
+        genie_songs = genie_api.get_album_songs(album_id)
         if not genie_songs:
             self.cache[track_release.id] = None
             return None
@@ -162,53 +105,3 @@ class Genie(Provider):
         self.cache[track_release.id] = mapped_songs
 
         return mapped_songs
-
-    @staticmethod
-    def fetch_genie_album_songs(album_id: int) -> list[GenieSong] | None:
-        response = httpx.get(GENIE_ALBUM_API_URL.format(album_id=album_id), headers={'User-Agent': CURL_USER_AGENT})
-        try:
-            response_json = response.json()
-        except JSONDecodeError:
-            return None
-
-        try:
-            songs = list(response_json['DATA1']['DATA'])
-        except KeyError:
-            return None
-
-        # Reject albums with multiple CDs
-        if any(song.get('ALBUM_CD_NO') != '1' for song in songs):
-            return None
-
-        # Extract songs
-        result = []
-
-        for song in songs:
-            song_id = song.get('SONG_ID')
-            track_num = song.get('ALBUM_TRACK_NO')
-            song_name = song.get('SONG_NAME')
-
-            if song_id is None or track_num is None or song_name is None:
-                return None
-
-            try:
-                song_id = int(song_id)
-                track_num = int(track_num)
-            except ValueError:
-                return None
-
-            result.append(GenieSong(id=song_id, album_index=track_num, title=unquote(song_name)))
-
-        result = sorted(result, key=lambda x: x.album_index)
-
-        return result
-
-    @staticmethod
-    def fetch_genie_stream_info(song_id: int) -> dict | None:
-        try:
-            return httpx.get(
-                GENIE_STREAM_INFO_API_URL.format(song_id=song_id),
-                headers={'User-Agent': CURL_USER_AGENT},
-            ).json()
-        except JSONDecodeError:
-            return None
