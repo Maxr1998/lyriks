1a94d9a4e760da6b2a3405837f0c174ce9a052b9
Maxr1998 <max.rumpf1998@gmail.com>
2025-12-30 14:14:06 +0100

    Extract matching of songs to recordings to base provider class

--- lyriks/providers/__init__.py
+++ lyriks/providers/__init__.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Callable, TypeVar
 
 from lyriks.lyrics import Lyrics
 from lyriks.mb_client import Release, Artist
+from .util import pick_release_from_release_group
 
 
 @dataclass
@@ -14,6 +16,10 @@ class Song(ABC):
     id: int
     album_index: int
     title: str
+
+
+T = TypeVar('T')
+S = TypeVar('S', bound=Song)
 
 
 class Provider(ABC):
@@ -42,7 +48,7 @@ class Provider(ABC):
         pass
 
     @abstractmethod
-    def fetch_song_by_id(self, song_id: int | str) -> Song | None:
+    def fetch_song_by_id(self, song_id: int) -> Song | None:
         """
         Fetch a provider-specific song entity by its ID.
         """
@@ -62,3 +68,60 @@ class Provider(ABC):
         :return: True if we're unable to check or if this artist has a URL, False otherwise.
         """
         pass
+
+    def get_mapped_provider_songs(
+        self, track_release: Release, selector: Callable[[Release], T | None], fetcher: Callable[[T], list[S] | None]
+    ) -> dict[str, S] | None:
+        """
+        Get songs for a track release, matched to its recordings.
+
+        :param track_release: The release to fetch and map songs for.
+        :param selector: A lambda function extracting a provider-specific identifier from a release if available.
+        :param fetcher: A function that takes the provider-specific identifier and returns a list of songs.
+        :return: A dictionary mapping recording MBIDs to provider-specific songs, or None if there was an error.
+        """
+        if track_release.id in self.cache:
+            return self.cache[track_release.id]
+
+        result = pick_release_from_release_group(track_release, selector)
+        if not result:
+            print(f'\rNo URL found for release {track_release.title} [{track_release.id}]')
+            self.cache[track_release.id] = None
+            self.missing_releases[track_release.id] = track_release
+            return None
+        matched_release, album_id = result
+
+        provider_songs = fetcher(album_id)
+        if not provider_songs:
+            self.cache[track_release.id] = None
+            return None
+
+        # Ensure track count matches
+        if len(provider_songs) != matched_release.get_track_count():
+            print(f'\rTrack count mismatch for release {track_release.title} [{track_release.id}]')
+            self.cache[track_release.id] = None
+            return None
+
+        # Match recordings to songs
+        mapped_songs = {}
+
+        # Iterate over all tracks in the release
+        for medium in matched_release.media:
+            for track in medium['tracks']:
+                recording_mbid: str = track['recording']['id']
+                try:
+                    # Match song by track number if possible
+                    track_number = int(track['number'])
+                    song = next(song for song in provider_songs if song.album_index == track_number)
+                except (ValueError, StopIteration):
+                    # Fall back to track position
+                    track_index = track['position'] - 1
+                    if track_index >= len(provider_songs):
+                        continue
+                    song = provider_songs[track_index]
+
+                mapped_songs[recording_mbid] = song
+
+        self.cache[track_release.id] = mapped_songs
+
+        return mapped_songs
--- lyriks/providers/genie.py
+++ lyriks/providers/genie.py
@@ -1,9 +1,8 @@
 from lyriks.lyrics import Lyrics
 from lyriks.mb_client import Release, Artist
-from . import Provider, Song
+from . import Provider
 from .api import genie_api
 from .api.genie_api import GenieSong
-from .util import pick_release_from_release_group
 
 
 class Genie(Provider):
@@ -13,7 +12,11 @@ class Genie(Provider):
 
     def fetch_recording_lyrics(self, track_release: Release, recording_mbid: str) -> Lyrics | None:
         # Resolve Genie album
-        genie_songs = self.get_genie_songs(track_release)
+        genie_songs = self.get_mapped_provider_songs(
+            track_release,
+            lambda r: r.get_genie_album_id(),
+            genie_api.get_album_songs,
+        )
         if not genie_songs:
             return None
 
@@ -25,7 +28,7 @@ class Genie(Provider):
         # Fetch lyrics
         return self.fetch_provider_song_lyrics(genie_song)
 
-    def fetch_song_by_id(self, song_id: int) -> Song | None:
+    def fetch_song_by_id(self, song_id: int) -> GenieSong | None:
         stream_info = genie_api.get_stream_info(song_id)
         if stream_info is None:
             return None
@@ -53,55 +56,3 @@ class Genie(Provider):
             self.missing_artists[artist.id] = artist
 
         return False
-
-    def get_genie_songs(self, track_release: Release) -> dict[str, GenieSong] | None:
-        """
-        Get Genie songs for a track release, matched to recordings.
-
-        :return: A dictionary mapping recording MBIDs to Genie songs, or None if there was an error.
-        """
-        if track_release.id in self.cache:
-            return self.cache[track_release.id]
-
-        result = pick_release_from_release_group(track_release, lambda r: r.get_genie_album_id())
-        if not result:
-            print(f'\rNo Genie URL found for release {track_release.title} [{track_release.id}]')
-            self.cache[track_release.id] = None
-            self.missing_releases[track_release.id] = track_release
-            return None
-        matched_release, album_id = result
-
-        genie_songs = genie_api.get_album_songs(album_id)
-        if not genie_songs:
-            self.cache[track_release.id] = None
-            return None
-
-        # Ensure track count matches
-        if len(genie_songs) != matched_release.get_track_count():
-            print(f'\rTrack count mismatch for release {track_release.title} [{track_release.id}]')
-            self.cache[track_release.id] = None
-            return None
-
-        # Match recordings to Genie songs
-        mapped_songs = {}
-
-        # Iterate over all tracks in the release
-        for medium in matched_release.media:
-            for track in medium['tracks']:
-                recording_mbid: str = track['recording']['id']
-                try:
-                    # Match song by track number if possible
-                    track_number = int(track['number'])
-                    song = next(song for song in genie_songs if song.album_index == track_number)
-                except (ValueError, StopIteration):
-                    # Fall back to track position
-                    track_index = track['position'] - 1
-                    if track_index >= len(genie_songs):
-                        continue
-                    song = genie_songs[track_index]
-
-                mapped_songs[recording_mbid] = song
-
-        self.cache[track_release.id] = mapped_songs
-
-        return mapped_songs
